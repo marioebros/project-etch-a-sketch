50e3ab34b7ebe53bce4f92e99147a4cbc37f95f6
Mario Elizaldegit config --global user.email dollarize.mme@gmail.com <dollarize.mme@gmail.com>
2022-07-08 14:15:23 -0700

    Added script to html

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
-let board = document.querySelector(".board");
-board.getElementsByClassName.gridTemplateColumns = "repeat(16, 1fr)";
-board.getElementsByClassName.gridTemplateRows = "repeat(16, 1fr)";
+function populateBoard(size) {
+  let board = document.querySelector(".board");
+  let squares = board.querySelectorAll("div");
+  squares.forEach((div) => div.remove());
+  board.style.gridTemplateColumns = `repeat(${size} , 1fr)`;
+  board.style.gridTemplateRows = `repeat(${size} , 1fr)`;
 
-for (let i = 0; i < 256; i++) {
-  let square = document.createElement("div");
-  square.style.backgroundColor = "pink";
-  board.insertAdjacentElement("beforeend", square);
+  for (let i = 0; i < 256; i++) {
+    let square = document.createElement("div");
+    square.style.backgroundColor = "pink";
+    board.insertAdjacentElement("beforeend", square);
+  }
+}
+
+populateBoard(16);
+
+function changeSize(input) {
+  populateBoard(input);
 }
